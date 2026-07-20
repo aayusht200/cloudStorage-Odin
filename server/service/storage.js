04ea6f6d609531291d749bbee2c616ece7d3ce26
Aayush Trivedi <aayush.t200@gmail.com>
2026-07-20 23:19:17 +0530

    Add S3 request presigner for generating signed URLs and refactor uploadFile function

--- server/service/storage.js
+++ server/service/storage.js
@@ -1,30 +1,30 @@
 import client from '../config/supabase.config.js';
 import { PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-// const params = {
-//     Bucket: process.env.S3_BUCKET_NAME,
-//     Key: req.file.originalname,
-//     Body: req.file.buffer,
-//     ContentType: req.file.mimetype,
-// };
-
-function uploadFile(req, res, next) {
+async function uploadFile(req, res, next) {
     const params = {
         Bucket: process.env.S3_BUCKET_NAME,
         Key: req.file.originalname,
         Body: req.file.buffer,
         ContentType: req.file.mimetype,
     };
+    const input = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: req.file.originalname,
+    };
     const command = new PutObjectCommand(params);
-    client
-        .send(command)
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    res.send({ message: 'test' });
+    try {
+        const uploadData = await client.send(command);
+        if (uploadData.$metadata.httpStatusCode === 200) {
+            const getInfo = new GetObjectCommand(input);
+            const url = await getSignedUrl(client, getInfo, { expiresIn: 3600 });
+            return res.status(200).send(JSON.stringify(url));
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(404).send({ message: 'Error' });
+    }
 }
 
 export default uploadFile;
