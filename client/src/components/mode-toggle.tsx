aa04ddf9976b7e62896371f9ff2655e59e5ecfea
Aayush Trivedi <aayush.t200@gmail.com>
2026-07-23 00:27:05 +0530

    feat: refactor theme management: move ThemeProvider to context, update imports, and enhance ModeToggle component

--- client/src/components/mode-toggle.tsx
+++ client/src/components/mode-toggle.tsx
@@ -1,25 +1,22 @@
 import { Moon, Sun } from "lucide-react";
 
-import { useTheme } from "../components/theme-provider";
-import { Button } from "../components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../components/ui/dropdown-menu";
+import { useTheme } from "../context/theme-provider";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+      <DropdownMenuTrigger className="inline-flex h-10 w-10 items-center justify-center rounded-md border">
+        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <span className="sr-only">Toggle theme</span>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
