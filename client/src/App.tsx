6ab5edce2f8a79e36b302e124d9b9c95bb4cf914
Aayush Trivedi <aayush.t200@gmail.com>
2026-07-22 22:19:27 +0530

    feat: add login page and update dependencies
    - Introduced a new Login component for user authentication.
    - Updated package.json to include new dependencies for UI components and styling.
    - Enhanced App component to utilize ThemeProvider and ModeToggle for theme management.
    - Refactored App.css to include new styles and themes.
    - Adjusted TypeScript configuration to support path aliases.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,18 @@
-import React, { useState, type ChangeEvent } from 'react';
-
-export default function App(): React.JSX.Element {
-    const [file, setFile] = useState<File | null>(null);
-
-    // Type the file input change event
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>): void => {
-        if (e.target.files && e.target.files.length > 0) {
-            setFile(() => e.target.files[0]);
-        }
-        console.log(file);
-    };
-
-    const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>): Promise<void> => {
-        e.preventDefault();
-        if (!file) return alert('Please select a file first');
-
-        const formData = new FormData();
-        // 'file' matches the key name your backend expects
-        formData.append('file', file);
-
-        try {
-            const response = await fetch('http://localhost:3000/', {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (!response.ok) {
-                throw new Error(`Upload failed with status: ${response.status}`);
-            }
-
-            const result = await response.json();
-            console.log('Success:', result);
-        } catch (error) {
-            console.error('Error uploading file:', error);
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
-            <input type="file" name="avatar" onChange={handleFileChange} />
-            <button type="submit">Submit</button>
-        </form>
-    );
+import { ModeToggle } from "../components/mode-toggle";
+import { ThemeProvider } from "../components/theme-provider";
+import { Login } from "../pages/Login";
+import "./App.css";
+function App() {
+  return (
+    <div className="bg-background flex h-dvh items-center justify-center">
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className="theme absolute top-1 right-1 z-10">
+          <ModeToggle />
+        </div>
+        <Login />
+      </ThemeProvider>
+    </div>
+  );
 }
+
+export default App;
