a8a3cf02c7ca9a7aa535022e7819bc896ad91f0d
Aayush Trivedi <aayush.t200@gmail.com>
2026-08-25 12:01:01 +0530

    fix: update ErrorPage button text and improve accessibility with aria-label

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -8,7 +8,10 @@ export default function ErrorPage() {
   const navigate = useNavigate();
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="bg-background flex h-dvh flex-col items-center justify-center gap-10">
+      <div
+        className="bg-background flex h-dvh flex-col items-center justify-center gap-10"
+        aria-label="Error Page"
+      >
         <div className="absolute top-1 right-1 z-10">
           <ModeToggle />
         </div>
@@ -18,12 +21,15 @@ export default function ErrorPage() {
           autoplay
           className="aspect-square h-60 place-self-center md:h-100 lg:h-120 lg:w-240"
         />
+        <h1 className="font-heading text-primary text-2xl">
+          Something went wrong
+        </h1>
         <Button
           variant="default"
           onClick={() => navigate("/", { replace: true })}
           className="cursor-pointer"
         >
-          Go Back
+          Go to Home
         </Button>
       </div>
     </ThemeProvider>
