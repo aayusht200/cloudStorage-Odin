d38cb147881c95858a9f392fd05797e187b6ca8d
Aayush Trivedi <aayush.t200@gmail.com>
2026-08-31 13:54:48 +0530

    feat: simplify signup API call in login test

--- client/tests/E2E/login.spec.ts
+++ client/tests/E2E/login.spec.ts
@@ -3,10 +3,7 @@ import axios from "axios";
 import createSignupPayload from "./createSignupPayload";
 test("should navigate to homepage on succesfull login", async ({ page }) => {
   const payload = createSignupPayload();
-  const response = await axios.post(
-    "http://localhost:3000/api/users/signup",
-    payload,
-  );
+  await axios.post("http://localhost:3000/api/users/signup", payload);
   page.on("response", async (response) => {
     if (response.url().includes("/api/")) {
       console.log("API:", response.status(), response.url());
