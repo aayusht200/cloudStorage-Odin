ec15f1151c97b683e7ffa9626897f24e43a93c47
Aayush Trivedi <aayush.t200@gmail.com>
2026-08-31 15:24:20 +0530

    feat: simplify E2E test by removing unnecessary logging and updating test command

--- client/tests/E2E/login.spec.ts
+++ client/tests/E2E/login.spec.ts
@@ -4,18 +4,11 @@ import createSignupPayload from "./createSignupPayload";
 test("should navigate to homepage on succesfull login", async ({ page }) => {
   const payload = createSignupPayload();
   await axios.post("http://localhost:3000/api/users/signup", payload);
-  page.on("response", async (response) => {
-    if (response.url().includes("/api/")) {
-      console.log("API:", response.status(), response.url());
-    }
-  });
   await page.goto("/login");
 
   await page.getByRole("textbox", { name: "Email" }).fill(payload.email);
   await page.getByRole("textbox", { name: "Password" }).fill(payload.password);
   await page.getByRole("button", { name: "Login" }).click();
-  console.log("URL:", page.url());
 
   await expect(page).toHaveURL(/\/drive\/[^/]+$/);
-
 });
